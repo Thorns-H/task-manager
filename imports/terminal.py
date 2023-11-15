c074b82ed432fb06bed315d94b440558466c616c
Thorns-H <abrahamm9986@gmail.com>
2023-11-15 17:28:39 -0600

    Added working FCFS visualization

--- imports/terminal.py
+++ imports/terminal.py
@@ -1,11 +1,14 @@
 import sys
+import time
 
 # Codigo ASNI para resetear los colores usados con otras secuencias de escape.
+
 reset_color = '\033[0m'
+important_refences = [False, 0]
 
-#Función encargada de imprimir las barras de progreso de nucleos y memoria ram. 
+# Función encargada de imprimir las barras de progreso de nucleos y memoria ram. 
 
-def display_bars(index, value, line, column) -> None:
+def display_bars(index: str | int, value: int, line: int, column: int) -> None:
 
     # Segun el valor, el color interno de la barra puede cambiar a rojo o verde.
 
@@ -15,3 +18,34 @@ def display_bars(index, value, line, column) -> None:
 
     sys.stdout.write(f'\033[{line};{column}H{bar}\n')
     sys.stdout.flush()
+
+def fill_bars(burst: int, line: int, column: int) -> None:
+
+    # Segun el valor, el color interno de la barra puede cambiar a rojo o verde.
+
+    color = '\033[32m'
+
+    for i in range(burst):
+        fill = color + '|' * (i + 1) + ' ' * (20 - burst) + reset_color
+        sys.stdout.write(f'\033[{line};{column}H{fill}\n')
+        sys.stdout.flush()
+
+        time.sleep(0.3)
+
+        important_refences[1] += 1
+        sys.stdout.write(f'\033[{5};{68}H\033[36mGlobal Time:\033[0m \033[92m{important_refences[1]}s\033[0m\n')
+        sys.stdout.flush()
+
+    sys.stdout.write(f'\033[{line};{61}H\033[32mDone{reset_color}\n')
+
+def display_processes(processes: dict) -> None:
+
+    for process in processes:
+        display_bars(f'P{process + 1}', 0, 6 + process, 30)
+
+    time.sleep(1)
+
+    for process, burst in processes.items():
+        fill_bars(burst[0], 6 + process, 39)
+
+    important_refences[0] = True
--- main.py
+++ main.py
@@ -1,16 +1,65 @@
 
 import sys
+import os
+import platform
+
 import time
 import random
 
 import threading
 
 from imports.config import load_config
-from imports.terminal import display_bars, display_processes, reset_color
+from imports.terminal import display_bars, display_processes, reset_color, important_refences
 
 threads = []
 
+clear_screen = "clear" if platform.system() == "Linux" else "cls" if platform.system() == "Windows" else None
+
+def usage_simulation(num_cores: int, num_ram: int, core_bars: list, ram_bars: list) -> None:
+
+    while not important_refences[0]:
+
+        # Generamos los números randoms.
+
+        for i in range(num_cores):
+            core_bars[i] = random.randint(0, 20)
+
+        for i in range(num_ram):
+            ram_bars[i] = random.randint(0, 15)
+
+        # Imprimimos el label de los nucleos del procesador.
+
+        sys.stdout.write(f'\033[{5};0H          \033[36mProcessor Cores{reset_color}\n')
+        sys.stdout.flush()
+
+        # Imprimimos las barras que simulan el uso de los nucleos.
+
+        for i in range(num_cores):
+            display_bars(i + 1, core_bars[i], i + 6, 0)
+
+        time.sleep(0.5)
+
+        # Imprimimos el label de el uso de memoria ram.
+
+        sys.stdout.write(f'\033[{7 + num_cores};0H            \033[36mMemory Usage{reset_color}\n')
+        sys.stdout.flush()
+
+        # Imprimimos las barras que simulan el consumo de la ram.
+
+        for i in range(num_ram):
+            display_bars(i + 1, ram_bars[i], i + num_cores + 8, 0)
+
+        time.sleep(0.5)
+
+        sys.stdout.write(f'\033[{5};30H          \033[36m Current Processes{reset_color}\n')
+        sys.stdout.flush()
+
+    sys.stdout.write(f'\033[{0};{0}H')
+    sys.stdout.flush()
+
 def main() -> None:
+
+    global keyboard_interrupted
 
     # Inicializamos los datos de la simulación.
 
@@ -41,8 +90,11 @@ def main() -> None:
     for i in range(num_processes):
         while True:
             try:
-                process_size = int(input(f"Ingrese el tamaño del proceso {i + 1}: "))
-                break
+                process_size = int(input(f"Ingrese el tamaño del proceso {i + 1} (máximo 20): "))
+                if 0 < process_size <= 20:
+                    break
+                else:
+                    print("Por favor, ingrese un número mayor a 0 y menor o igual a 20.")
             except ValueError:
                 print("Por favor, ingrese un número entero válido para el tamaño del proceso.")
 
@@ -53,49 +105,18 @@ def main() -> None:
     core_bars = [0] * num_cores
     ram_bars = [0] * num_ram
 
-    # Iniciamos el ciclo principal.
+    os.system(clear_screen)
 
-    while True:
+    thread = threading.Thread(target = usage_simulation, args = (num_cores, num_ram, core_bars, ram_bars))
+    threads.append(thread)
 
-        # Generamos los números randoms.
+    thread = threading.Thread(target = display_processes, args = (processes,))
+    threads.append(thread)
 
-        for i in range(num_cores):
-            core_bars[i] = random.randint(0, 20)
-
-        for i in range(num_ram):
-            ram_bars[i] = random.randint(0, 15)
-
-        # Imprimimos el label de los nucleos del procesador.
-
-        sys.stdout.write(f'\033[{5};0H          \033[36mProcessor Cores{reset_color}\n')
-        sys.stdout.flush()
-
-        # Imprimimos las barras que simulan el uso de los nucleos.
-
-        for i in range(num_cores):
-            display_bars(i + 1, core_bars[i], i + 6, 0)
-
-        # Imprimimos el label de el uso de memoria ram.
-
-        sys.stdout.write(f'\033[{7 + num_cores};0H            \033[36mMemory Usage{reset_color}\n')
-        sys.stdout.flush()
-
-        # Imprimimos las barras que simulan el consumo de la ram.
-
-        for i in range(num_ram):
-            display_bars(i + 1, ram_bars[i], i + num_cores + 8, 0)
-
-        # Movemos a la altura del primer label y movemos 30 columnas a la derecha.
-        # Después imprimimos un label de procesos.
-
-        sys.stdout.write(f'\033[5;30H            \033[36m Processes{reset_color}\n')
-        sys.stdout.flush()
-
-        # Imprimimos las barras de procesos.
-
-        # TODO: Falta agregar una función que siga la lógica de 'display_bars' pero
-        # no tome un número random, sino un algoritmo como FCFS, RR o Queues.
-
-        input()
+    for thread in threads:
+        thread.start()
+        
+    for thread in threads:
+        thread.join()
 
 main()
